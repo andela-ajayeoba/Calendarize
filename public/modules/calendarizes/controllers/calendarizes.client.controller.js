6c6927f810ab4a94f856599ea7a491c830615d4d
atumachimela <chimelatuma@yahoo.com>
2014-10-30 23:34:24 -0500

    modal testing

--- public/modules/calendarizes/controllers/calendarizes.client.controller.js
+++ public/modules/calendarizes/controllers/calendarizes.client.controller.js
@@ -1,10 +1,37 @@
 'use strict';
 
 // Calendarizes controller
-angular.module('calendarizes').controller('CalendarizesController', ['$scope', '$stateParams', '$location', 'Authentication', 'Calendarizes',
-	function($scope, $stateParams, $location, Authentication, Calendarizes ) {
+angular.module('calendarizes').controller('CalendarizesController', ['$scope','$modal','$log','$modalInstance','items','$stateParams', '$location', 'Authentication', 'Calendarizes',
+	function($scope,$modal,$modalInstance, items,$stateParams,$location,Authentication,Calendarizes ) {
 		$scope.authentication = Authentication;
+		var ModalDemoCtrl = function ($scope, $modal) {
+ 		$scope.open = function (size) {
+    	var modalInstance = $modal.open({
+      	templateUrl: 'myModalContent.html',
+      	controller: CalendarizesController,
+      	size: size
+    });
+    modalInstance.result.then(function (take_me_outside) {
+      $scope.message = take_me_outside;
+    });
+  };
+};
 
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+var ModalInstanceCtrl = function ($scope, $modalInstance) {
+  $scope.take_me_outside = "asdfasdf";
+  
+  $scope.ok = function (take_me_outside) {
+    console.log($modalInstance);
+    $modalInstance.close(take_me_outside);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
 		// Create new Calendarize
 		$scope.create = function() {
 			// Create new Calendarize object
@@ -61,5 +88,6 @@ angular.module('calendarizes').controller('CalendarizesController', ['$scope', '
 				calendarizeId: $stateParams.calendarizeId
 			});
 		};
+		
 	}
 ]);
