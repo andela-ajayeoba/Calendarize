3acbc24922de7e0822fc0f96a250c6a20f76f064
atumachimela <chimelatuma@yahoo.com>
2014-11-06 13:31:40 -0600

    create and list worker

--- public/modules/calendarizes/controllers/calendarizes.client.controller.js
+++ public/modules/calendarizes/controllers/calendarizes.client.controller.js
@@ -4,102 +4,20 @@
 angular.module('calendarizes').controller('CalendarizesController', ['$scope','$modal','$stateParams', '$location', 'Authentication', 'Apicall',
 	function($scope,$modal,$stateParams,$location, Authentication, Apicall ) {
 		$scope.authentication = Authentication;
-		//testing modal
-		// var ModalDemoCtrl = function ($scope, $modal) {
-		$scope.open = function (size) {
-	    	var modalInstance = $modal.open({
-		      	templateUrl: 'myModalContent.html',
-		      	controller: ModalInstanceCtrl,
-		      	size: size
-		    });
-		    modalInstance.result.then(function (take_me_outside) {
-		      $scope.message = take_me_outside;
-		    });
-		};
-	var ModalInstanceCtrl = function ($modalInstance, $scope) {
-			$scope.take_me_outside = 'asdfasdf';
-		$scope.ok = function (take_me_outside) {
-			$modalInstance.close(take_me_outside);
-		};
-		$scope.cancel = function () {
-		    $modalInstance.dismiss('cancel');
-	  	};
-	};
-
-		// // Create new Calendarize
-		// $scope.create = function() {
-		// 	// Create new Calendarize object
-		// 	var calendarize = new Calendarizes ({
-		// 		name: this.name
-		// 	});
-
-		// 	// Redirect after save
-		// 	calendarize.$save(function(response) {
-		// 		$location.path('calendarizes/' + response._id);
-
-		// 		// Clear form fields
-		// 		$scope.name = '';
-		// 	}, function(errorResponse) {
-		// 		$scope.error = errorResponse.data.message;
-		// 	});
-		// };
-
-		// // Remove existing Calendarize
-		// $scope.remove = function( calendarize ) {
-		// 	if ( calendarize ) { calendarize.$remove();
-
-		// 		for (var i in $scope.calendarizes ) {
-		// 			if ($scope.calendarizes [i] === calendarize ) {
-		// 				$scope.calendarizes.splice(i, 1);
-		// 			}
-		// 		}
-		// 	} else {
-		// 		$scope.calendarize.$remove(function() {
-		// 			$location.path('calendarizes');
-		// 		});
-		// 	}
-		// };
-
-		// // Update existing Calendarize
-		// $scope.update = function() {
-		// 	var calendarize = $scope.calendarize ;
-
-		// 	calendarize.$update(function() {
-		// 		$location.path('calendarizes/' + calendarize._id);
-		// 	}, function(errorResponse) {
-		// 		$scope.error = errorResponse.data.message;
-		// 	});
-		// };
-
-		// // Find a list of Calendarizes
-		// $scope.find = function() {
-		// 	$scope.calendarizes = Calendarizes.query();
-		// };
-
-		// // Find existing Calendarize
-		// $scope.findOne = function() {
-		// 	$scope.calendarize = Calendarizes.get({ 
-		// 		calendarizeId: $stateParams.calendarizeId
-		// 	});
-		// };
-
+		console.log('test')
 		/************************************************
 					WORKERS CRUD
 		************************************************/
-
-		//Response to action message
-		var responseMessage = function(title, action){
-			$scope.msg = title+ ' is successfully '+ action;
-		};
 
 		// Creating a new worker
 		$scope.addWorker = function() {
 			// Create new Calendarize object
 			var worker = new Apicall.Workers ($scope.worker);
-
+				console.log('hello');
+				console.log(worker);
+				console.log($scope.worker);
 			// Redirect after save
 			worker.$save(function(response) {
-				// $location.path('calendarizes/' + response._id);
 					$scope.msg = 'Worker Successfully added';
 				// Clear form fields
 				$scope.worker = '';
---
+++ public/modules/calendarizes/directives/custompopover.client.directive.js
@@ -0,0 +1,18 @@
+// 'use strict';
+
+// angular.module('calendarizes').directive('custompopover', [
+// 	return {
+//         restrict: 'A',
+//         template: '<span>{{label}}</span>',
+//         link: function (scope, el, attrs) {
+//             scope.label = attrs.popoverLabel;
+
+//             $(el).popover({
+//                 trigger: 'click',
+//                 html: true,
+//                 content: attrs.popoverHtml,
+//                 placement: attrs.popoverPlacement
+//             });
+//         }
+//     };
+// ]);
