f9818e995316a171b5a5ea230140a351a21262c6
atumachimela <chimelatuma@yahoo.com>
2014-11-11 20:39:36 +0000

    modal test

--- public/modules/calendarizes/controllers/calendarizes.client.controller.js
+++ public/modules/calendarizes/controllers/calendarizes.client.controller.js
@@ -3,6 +3,53 @@
 angular.module('calendarizes').controller('CalendarizesController', ['$scope','$stateParams', '$location', '$timeout','Authentication', 'Apicall','Uuid', 'Sample', 'moment', 'GANTT_EVENTS',
 	function($scope,$stateParams, $location, $timeout, Authentication, Apicall, Uuid, Sample, moment, GANTT_EVENTS ) {
 		$scope.authentication = Authentication;
+	    $scope.showModal = false;
+	    $scope.toggleModal = function(){
+	    	console.log("yeoman");
+	    $scope.showModal = !$scope.showModal;
+	    };
+calendarizes.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
+		// Modal Test Ends 
         /* Create a new person */
         $scope.addPerson = function() {
             var person = new Apicall.Persons($scope.person);
@@ -248,9 +295,10 @@ angular.module('calendarizes').controller('CalendarizesController', ['$scope','$
         });
 
         // function that trigers popover onclick on the gantt chart cells
-        $scope.$on(GANTT_EVENTS.ROW_CLICKED,function(){
-        	//popover code 
+       $scope.$on(GANTT_EVENTS.ROW_CLICKED,function(){
+        	//show popover code 
         	console.log("test");
+
        	});
         
         $scope.$on(GANTT_EVENTS.READY, function() {
