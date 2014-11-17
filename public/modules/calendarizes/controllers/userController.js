6a54bdcd2f73e09ba6d58ff5a2f9eef1dcb02abc
OLAJIDE ADERIBIGBE <olajideaderibigbe@OLAJIDEs-MacBook-Pro.local>
2014-11-17 11:09:51 +0100

    Prettified code

--- public/modules/calendarizes/controllers/userController.js
+++ public/modules/calendarizes/controllers/userController.js
@@ -1,9 +1,8 @@
 'use strict';
-angular.module('calendarizes').controller('userLoginController', function($scope, $rootScope){
-  $scope.user=  {};
-  
-  $scope.authenticate = function(){
-    $rootScope.$broadcast('login');
-  };
-});
+angular.module('calendarizes').controller('userLoginController', function($scope, $rootScope) {
+    $scope.user = {};
 
+    $scope.authenticate = function() {
+        $rootScope.$broadcast('login');
+    };
+});
