723dab85c9a0dbdb65fbda1711adba696c13fa07
andela-ajayeoba <dejijaye@gmail.com>
2014-11-20 20:00:47 +0100

    removed useless module

--- public/modules/calendarizes/controllers/userController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('calendarizes').controller('userLoginController', function($scope, $rootScope){
-  $scope.user=  {};
-  
-  $scope.authenticate = function(){
-    $rootScope.$broadcast('login');
-  };
-});
-
